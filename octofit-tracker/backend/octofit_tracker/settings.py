ef9fd688337f71fe2b57de66e38280d6782b64c4
DanielSurridge <daniel.surridge@equiniti.com>
2025-12-01 15:49:29 +0000

    Update ALLOWED_HOSTS and API endpoint URLs for Codespace and localhost support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-fk_%@6ct$^+&s4*4c3yi*1@3l^p1ks%)soawl0peneob!1nv8r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
